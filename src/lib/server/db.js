138f44b5dfe706a898c833995704e787e19bec97
udu3324 <47045986+udu3324@users.noreply.github.com>
2026-03-22 16:05:11 -0500

    switch port

--- src/lib/server/db.js
+++ src/lib/server/db.js
@@ -1,7 +1,7 @@
 import { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOSTNAME, POSTGRES_DB } from '$env/static/private'
 import postgres from 'postgres'
 
-const sql = postgres(`postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOSTNAME}:5432/${POSTGRES_DB}`, {
+const sql = postgres(`postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOSTNAME}:6543/${POSTGRES_DB}`, {
     ssl: process.env.NODE_ENV === 'production' && process.env.POSTGRES_SSL === 'true'
 })
 
